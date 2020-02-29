590b4770bd1b9f61150e1d2e074e73a9cd358ea4
GuoJint <1298779616@qq.com>
2020-02-29 14:34:28 +0800

    首页导航

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="NavHeader">
-        header
+        <div class="container">
+            <div class="centerLeft">
+                <span @click="toHome">首页</span>
+                <span>我的博客</span>
+                <span>我的关注</span>
+            </div>
+            <div class="centerRight">
+                <span>我的中心</span>
+                <span>消息提示</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,8 +19,44 @@ export default {
     name: 'NavHeader',
     data() { 
         return {
-
+            nowRouterPath:this.$route.name,
+        }
+    },
+    methods:{
+        toHome(){
+            console.log(this.nowRouterPath)
+            if(this.nowRouterPath  !== "articleShow"){
+                console.log("ss")
+                this.$router.push("/articleShow")
+            }
         }
     }
 }
 </script>
+<style lang="scss">
+.NavHeader{
+    width: 100%;
+    height: 60px;
+    background-color: #409EFF;
+    .container{
+        width: 1208px;
+        height: 60px;
+        margin: 0 auto;
+        background-color: #409EFF;
+        span{
+            margin-right:15px; 
+        }
+        span:hover{
+            cursor: pointer;
+        }
+        .centerLeft,.centerRight{
+            display: inline-block;
+            line-height: 60px;
+            color: #ECE6E6;
+        }
+        .centerRight{
+            float: right;
+        }
+    }
+}
+</style>
--- src/mock/api.js
+++ src/mock/api.js
@@ -6,6 +6,7 @@ Mock.setup({
     timeout: '400-800'
 })
 
+//登录注册
 Mock.mock('/api/login',{
     data:{
         token:Random.string('number',6),
@@ -19,5 +20,26 @@ Mock.mock('/api/register/code',{
 Mock.mock('/api/register',{
     data:{
         message:'注册成功'
+    }
+})
+
+//文章详情
+Mock.mock('/api/articleListShow',{
+    data:{
+
+        articleList:[
+            {
+                articleID:1001,
+                userID:12,
+                article:'Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。如果你想在深入学习 Vue 之前对它有更多了解，我们制作了一个视频，带您了解其核心概念和一个示例工程。如果你已经是有经验的前端开发者，想知道 Vue 与其它库/框架有哪些区别，请查看对比其它框架。'
+                
+            },
+            {
+                articleID:1015,
+                userID:31,
+                article:"这里我们遇到了一点新东西。你看到的 v-bind attribute 被称为指令。指令带有前缀 v-，以表示它们是 Vue 提供的特殊 attribute。可能你已经猜到了，它们会在渲染的 DOM 上应用特殊的响应式行为。在这里，该指令的意思是：“将这个元素节点的 title attribute 和 Vue 实例的 message 属性保持一致”。如果你再次打开浏览器的 JavaScript 控制台，输入 app2.message = '新消息'，就会再一次看到这个绑定了 title attribute 的 HTML 已经进行了更新。"
+                
+            }
+        ]
     }
 })
